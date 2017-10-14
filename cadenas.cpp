63c627b3abc73d4e108692d3815d0cbd66c8af7b
FabioCarbajal <fabio.carbajal@ucsp.edu.pe>
2017-10-14 11:32:01 -0500

    Comparacion de cadenas

--- cadenas.cpp
+++ cadenas.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void comparacion(char* a, char* b)
+void comparacion(char *a,char* b)
 {
     while((*a!='\0') && (*b!='\0'))
     {
-        if(*a<*b)
-        {
-            cout<<"menor";
-        }
         if(*a>*b)
         {
             cout<<"mayor";
+            break;
+        }
+        if(*a<*b)
+        {
+            cout<<"menor";
+            break;
+        }
+        if(*a==*b)
+        {
             a++;
             b++;
         }
+    }
+    if(*a=='\n' && *b=='\n')
+    {
+
     }
 }
 
 int main()
 {
-    char a[5]="hola";
-    char b[6]="anita";
-    comparacion(a,b);
+    string a,b;
+    char* p;
+    char* p1;
+    cout<<"Primera palabra: ";
+    cin>> a;
+    p=(char*) a.c_str();
+    cout<<"Segunda Palabra: ";
+    cin>> b;
+    p1=(char*) b.c_str();
+    comparacion(p,p1);
     return 0;
 }
