1b8f14b873816ab0a277c3c61fa58ffb675a8590
FabioCarbajal <fabio.carbajal@ucsp.edu.pe>
2017-11-24 15:12:28 -0500

    Josephus

--- main.cpp
+++ main.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
+class Node{
+public:
+	int value;
+	Node *next;
+	Node(int val, Node *sigue){
+		this->value = val;
+		this->next = sigue;
+	}
+};
+typedef Node *linked
+
 int main()
 {
-    for(int i=2;i<=1000;i++){
-        int a=0;
-        for(int j=1;j<=1000;j++){
-            if(i%j==0)
-                a++;
+    int i, n=atoi()
+    linked sigue=new Node(1,NULL);
+    sigue->next=sigue;
+    linked val = sigue;
+    for(i=2;i<=n;i++)
+    {
+        val=(val->next=new Node(1,sigue));
+    }
+    cout<<"mueren";
+    while(val!=val->next)
+    {
+        for(i=1;i<k;i++)
+        {
+            val=val->next;
         }
-        if(a==2){
-            cout<< " " <<i;
-        }
+        sigue=val->next;
+        val->next=sigue->next;
     }
     return 0;
 }
